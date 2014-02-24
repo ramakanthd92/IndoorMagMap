4c2763fb170882c60534c3efb29c4056244243fe
ramakanth putta <ramakanthd92@gmail.com>
2014-02-24 15:08:04 +0530

    RV-LA

--- src/in/ernet/iitr/puttauec/sensors/SensorLifecycleManager.java
+++ src/in/ernet/iitr/puttauec/sensors/SensorLifecycleManager.java
@@ -53,7 +53,7 @@ public class SensorLifecycleManager {
 		mSensorManager.registerListener(mSensorEventListener, mSensorManager
 				.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
 				SENSOR_DELAY);
-		mSensorManager.registerListener(mSensorEventListener,
+	/*	mSensorManager.registerListener(mSensorEventListener,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
 				SENSOR_DELAY);  
 				
@@ -62,7 +62,7 @@ public class SensorLifecycleManager {
 				SENSOR_DELAY);
 		mSensorManager.registerListener(mSensorEventListener,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
-				SENSOR_DELAY); 
+				SENSOR_DELAY); */
 	}
 
 	@SuppressWarnings("unused")
